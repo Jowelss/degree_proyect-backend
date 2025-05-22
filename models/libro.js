c2c685c64f7776134648746b689d9b175a062823
Jowelss <devejowels.gitl.loper@gmail.com>
2025-05-21 21:31:49 -0400

    Elimine la propiedad de cantidad - Agregue comentario de como debe verse en la web para el usuario

--- models/libro.js
+++ models/libro.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 
 const libroSchema = new mongoose.Schema({
-  nombre: String,
-  autor: String,
-  genero: String,
+  nombre: String, // Nombre
+  autor: String, // Autor/a
+  genero: String, // Género
   imagen: {
     type: String,
     required: true,
   },
-  sinopsis: String,
-  precio: Number,
-  cantidad: Number,
-  estado: String,
+  sinopsis: String, // ¿De que trata?
+  precio: Number, // Inversion
+  estado: String, // Disponibles - Agotado
+  formato: String, // ¿Como es este ejemplar?
 });
 
 export default mongoose.model('Libro', libroSchema);
